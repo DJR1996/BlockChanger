2f186ef63a8db6566d0a4f80b837827a7f9cf276
Dylan <Dylan@Dylan-PC.woh.rr.com>
2013-08-15 21:18:23 -0400

    First Commit!

---
+++ BlockChanger/src/io/github/DJR1996/Main/BlockChanger.java
@@ -0,0 +1,15 @@
+package io.github.DJR1996.Main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BlockChanger extends JavaPlugin{
+	
+	public void onEnable(){
+		getLogger().info("BlockChanger has been enabled!");
+	}
+	
+	public void onDisable(){
+		getLogger().info("BlockChanger has been disabled!");
+	}
+
+}
